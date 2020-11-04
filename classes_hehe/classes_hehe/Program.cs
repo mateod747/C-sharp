b2163aca176927fb830e5244985f9c01dafe2b1c
mateo_dubinjak <mdubinjak@etfos.hr>
2020-11-04 13:46:51 +0100

    classes

---
+++ classes_hehe/classes_hehe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace classes_hehe
+{
+    class Burger
+    {
+        public string Make { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Burger cheeseBurger = new Burger();
+            cheeseBurger.Make = "Burger!";
+            cheeseBurger.Type = "Cheese";
+            cheeseBurger.Price = 2;
+
+            Console.WriteLine("{0} {1} {2}", cheeseBurger.Make, cheeseBurger.Type, "$" + cheeseBurger.Price);
+
+            if(IsBurgerMoreExpensiveThanTwoDollars(cheeseBurger))
+            {
+                Console.WriteLine("It's more expensive than $2");
+            } else
+            {
+                Console.WriteLine("It's not more expensive than $2");
+            }
+        }
+
+        private static bool IsBurgerMoreExpensiveThanTwoDollars(Burger burger)
+        {
+            if (burger.Price > 2) return true;
+            else return false;
+        }
+    }   
+}
