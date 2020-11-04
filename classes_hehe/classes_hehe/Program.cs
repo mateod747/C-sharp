916f2a4f7c0d5b8118e5df2d3b8208292834582c
mateo_dubinjak <mdubinjak@etfos.hr>
2020-11-04 15:09:19 +0100

    classes:update

--- classes_hehe/classes_hehe/Program.cs
+++ classes_hehe/classes_hehe/Program.cs
@@ -7,6 +7,18 @@ namespace classes_hehe
         public string Make { get; set; }
         public string Type { get; set; }
         public int Price { get; set; }
+
+        public Burger()
+        {
+            Make = "Hamburger";
+        }
+
+        public Burger(string make, string type, int price)
+        {
+            Make = make;
+            Type = type;
+            Price = price;
+        }
     }
     class Program
     {
@@ -26,6 +38,10 @@ namespace classes_hehe
             {
                 Console.WriteLine("It's not more expensive than $2");
             }
+
+            Burger hamBurger = new Burger("Burger...", "Ham", 3);
+
+            Console.WriteLine("{0} {1} {2}", hamBurger.Make, hamBurger.Type, "$" + hamBurger.Price);
         }
 
         private static bool IsBurgerMoreExpensiveThanTwoDollars(Burger burger)
