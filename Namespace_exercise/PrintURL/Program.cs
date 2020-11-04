e6f5400497c16ca755ad68a948cabf7e840948b3
mateo_dubinjak <mdubinjak@etfos.hr>
2020-11-04 15:29:28 +0100

    namespaces

---
+++ Namespace_exercise/PrintURL/Program.cs
@@ -0,0 +1,17 @@
+﻿using ScrapeLibrary;
+using System;
+
+namespace PrintURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+
+            string value = myScrape.ScrapeWebPage("https://www.youtube.com/");
+
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Namespace_exercise/ScrapeLibrary/Scrape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+
+        public string ScrapeWebPage(string url)
+        {
+            string value = GetWebPage(url);
+            value += "HEHEHEHE";
+            return value;
+        }
+
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            string value = GetWebPage(url);
+            value += "HEHEHEEHE";
+
+            File.WriteAllText(filepath, value);
+            return value;
+        }
+    }
+}
