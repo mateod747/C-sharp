eab2347bce83552ad4c3601345237dd39d22f5ea
mateo_dubinjak <mdubinjak@etfos.hr>
2020-11-05 14:11:13 +0100

    exceptions

---
+++ exceptions/exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string quote = File.ReadAllText(@"C:\Users\mateodubinjak\Desktop\example.txt");
+                Console.WriteLine(quote);
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
